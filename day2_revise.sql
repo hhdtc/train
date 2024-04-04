5527255e9912178cac37460dabb193420a60e00b
hhdtc <alex_hhdtc@outlook.com>
2024-04-04 18:30:40 -0400

    day3
    day3

---
+++ day2_revise.sql
@@ -0,0 +1,197 @@
+--USE AdventureWorks2019
+USE AdventureWorks2019
+GO
+
+--1
+SELECT COUNT(1)
+FROM Production.Product
+
+--2
+SELECT COUNT(1)
+FROM Production.Product p
+WHERE p.ProductSubcategoryID IS NOT NULL
+
+--3
+SELECT p.ProductSubcategoryID, COUNT(1) AS CountedProducts
+FROM Production.Product p
+GROUP BY p.ProductSubcategoryID
+HAVING p.ProductSubcategoryID IS NOT NULL
+
+--4
+SELECT COUNT(1)
+FROM Production.Product p
+WHERE p.ProductSubcategoryID IS NULL
+
+--5
+SELECT p.ProductID,SUM(p.Quantity) Quantity
+FROM Production.ProductInventory p
+GROUP BY p.ProductID
+
+
+
+--6
+SELECT p.ProductID,SUM(p.Quantity) TheSum
+FROM Production.ProductInventory p
+WHERE p.LocationID = 40
+GROUP BY p.ProductID
+HAVING SUM(p.Quantity)<100
+
+--7
+SELECT p.ProductID,p.Shelf,SUM(p.Quantity) TheSum
+FROM Production.ProductInventory p
+WHERE p.LocationID = 40
+GROUP BY p.ProductID, p.Shelf
+HAVING SUM(p.Quantity)<100
+
+--8
+SELECT p.ProductID,AVG(p.Quantity) TheAvg
+FROM Production.ProductInventory p
+WHERE LocationID = 10
+GROUP BY p.ProductID
+
+--9
+SELECT p.ProductID, p.Shelf, AVG(p.Quantity) TheAvg
+FROM Production.ProductInventory p
+GROUP BY p.ProductID, p.Shelf
+
+--10
+SELECT p.ProductID, p.Shelf, AVG(p.Quantity) TheAvg
+FROM Production.ProductInventory p
+WHERE p.Shelf != 'N/A'
+GROUP BY p.ProductID, p.Shelf
+
+--11
+SELECT p.Color, p.Class, COUNT(1) TheCount, AVG(P.ListPrice) AvgPrice
+FROM Production.Product p
+GROUP BY p.Color, p.Class
+
+--12
+SELECT cr.Name Country, sp.Name Province
+FROM person.CountryRegion cr
+JOIN person.StateProvince sp
+ON sp.CountryRegionCode = cr.CountryRegionCode
+
+--13
+SELECT cr.Name Country, sp.Name Province
+FROM person.CountryRegion cr
+JOIN person.StateProvince sp
+ON sp.CountryRegionCode = cr.CountryRegionCode
+WHERE cr.Name in ('Germany','Canada')
+
+
+--USE Northwind
+USE Northwind
+GO
+
+--14
+SELECT DISTINCT p.ProductName
+FROM Orders o
+JOIN [Order Details] od
+ON od.OrderID = o.OrderID
+JOIN Products p
+ON od.ProductID = p.ProductID
+WHERE o.OrderDate >= DATEADD(YEAR, -26, GETDATE())
+
+--15
+SELECT TOP 5 e.PostalCode
+FROM Orders o
+JOIN [Order Details] od
+ON od.OrderID = o.OrderID
+JOIN Employees e
+ON o.EmployeeID = e.EmployeeID
+Group BY e.PostalCode
+HAVING e.PostalCode IS NOT NULL
+ORDER BY SUM(od.Quantity) DESC
+
+--16
+
+
+SELECT TOP 5 e.PostalCode
+FROM Orders o
+JOIN [Order Details] od
+ON od.OrderID = o.OrderID
+JOIN Employees e
+ON o.EmployeeID = e.EmployeeID
+WHERE o.OrderDate >= DATEADD(YEAR, -26, GETDATE())
+Group BY e.PostalCode
+HAVING e.PostalCode IS NOT NULL
+ORDER BY SUM(od.Quantity) DESC
+
+
+--17
+SELECT c.City, COUNT(1) Num
+FROM Customers c
+GROUP BY c.City
+
+--18
+SELECT c.City, COUNT(1) Num
+FROM Customers c
+GROUP BY c.City
+HAVING COUNT(1) > 2
+
+--19
+SELECT c.CompanyName, o.OrderDate
+FROM Customers c
+JOIN Orders o
+ON o.CustomerID = c.CustomerID
+WHERE o.OrderDate > '1998-01-01'
+
+--20
+SELECT c.CompanyName, MAX(o.OrderDate)
+FROM Customers c
+JOIN Orders o
+ON o.CustomerID = c.CustomerID
+Group BY c.CompanyName
+
+--21
+SELECT c.CompanyName, SUM(od.Quantity) quantity
+FROM Customers c
+JOIN Orders o
+ON o.CustomerID = c.CustomerID
+JOIN [Order Details] od
+ON od.OrderID = o.OrderID
+Group BY c.CompanyName
+
+--22
+SELECT c.CustomerID, SUM(od.Quantity)
+FROM Customers c
+JOIN Orders o
+ON o.CustomerID = c.CustomerID
+JOIN [Order Details] od
+ON od.OrderID = o.OrderID
+Group BY c.CustomerID
+HAVING SUM(od.Quantity) > 100
+
+--23
+SELECT sup.CompanyName 'Supplier Company Name', ship.CompanyName 'Shipping Company Name'
+FROM Suppliers sup, Shippers ship
+
+--24
+SELECT DISTINCT o.OrderDate,p.ProductName
+FROM Orders o
+JOIN [Order Details] od
+ON od.OrderID = o.OrderID
+JOIN Products p
+ON od.ProductID = p.ProductID
+
+--25
+SELECT a.FirstName+' '+a.LastName AS 'Name1',  b.FirstName+' '+b.LastName AS 'Name2'
+FROM Employees a
+JOIN Employees b
+ON a.Title = b.Title
+WHERE a.EmployeeID != b.EmployeeID
+
+--26
+SELECT manager.FirstName+' '+manager.LastName AS 'Name', COUNT(reporter.EmployeeID) 'Employees Num'
+FROM Employees manager
+JOIN Employees reporter
+ON reporter.ReportsTo = manager.EmployeeID
+GROUP BY manager.FirstName+' '+manager.LastName
+HAVING COUNT(reporter.EmployeeID) > 2
+
+--27
+SELECT c.City AS 'City', c.CompanyName AS 'Name', c.ContactName AS 'Contact Name', 'Customer' AS 'Type'
+FROM Customers c
+UNION ALL
+SELECT s.City AS 'City', s.CompanyName AS 'Name', s.ContactName AS 'Contact Name', 'Supplier' AS 'Type'
+FROM Suppliers s
---
+++ day3.sql
@@ -0,0 +1,181 @@
+--1
+SELECT DISTINCT e.City
+FROM Employees e
+INNER JOIN Customers c
+ON e.City = c.City
+
+--2.a
+SELECT DISTINCT c.City
+FROM Customers c
+LEFT JOIN Employees e
+ON e.City = c.City
+WHERE e.EmployeeID IS NULL
+
+--2.b
+SELECT DISTINCT c.City
+FROM Customers c
+WHERE c.City NOT IN (
+    SELECT DISTINCT e.City
+    FROM Employees e
+)
+
+--3
+SELECT p.ProductName, SUM(od.Quantity) AS Sum
+FROM Orders o
+JOIN [Order Details] od
+ON o.OrderID = od.OrderID
+JOIN Products p
+ON od.ProductID = p.ProductID
+GROUP BY p.ProductName
+
+--4
+SELECT c.City, SUM(od.Quantity) AS Sum
+FROM Orders o
+JOIN [Order Details] od
+ON o.OrderID = od.OrderID
+JOIN Customers c
+ON o.CustomerID = c.CustomerID
+GROUP BY c.City
+
+--5.a
+SELECT DISTINCT c1.City
+FROM Customers c1
+JOIN Customers c2
+ON c1.City = c2.City
+WHERE c1.CustomerID != c2.CustomerID
+
+--5.b
+SELECT DISTINCT c1.City
+FROM Customers c1
+WHERE c1.City NOT IN (
+    SELECT c2.City
+    FROM Customers c2
+    GROUP BY c2.City
+    HAVING COUNT(c2.CustomerID)<2
+)
+
+--6
+SELECT DISTINCT c.City
+FROM Customers c
+JOIN Orders o
+ON o.CustomerID = c.CustomerID
+JOIN [Order Details] od
+ON od.OrderID = o.OrderID
+GROUP by c.City
+HAVING COUNT(DISTINCT od.ProductID)>= 2
+
+--7
+SELECT DISTINCT c.CompanyName
+FROM Customers c
+JOIN Orders o
+ON o.CustomerID = c.CustomerID
+WHERE o.ShipCity != c.City
+
+--8
+--STEPS:
+--Average Price
+SELECT od.ProductID,SUM(od.Quantity*od.UnitPrice*od.Quantity)/SUM(od.Quantity) AS AveragePrice
+FROM [Order Details] od
+GROUP BY od.ProductID
+
+--CustomersCity
+SELECT od.ProductID, c.City, RANK( ) OVER(PARTITION BY od.ProductID ORDER BY SUM(od.Quantity) DESC) AS Rank, SUM(od.Quantity) as sum
+FROM Orders o
+JOIN [Order Details] od
+ON o.OrderID = od.OrderID
+Join Customers c
+ON c.CustomerID = o.CustomerID
+GROUP BY od.ProductID,  c.City
+
+--TOP 5 popular product
+SELECT TOP 5 od.ProductID, RANK() OVER(ORDER BY SUM(od.Quantity)DESC) AS Rank
+FROM [Order Details] od
+GROUP BY od.ProductID
+ORDER BY SUM(od.Quantity)DESC
+
+--FINAL
+SELECT p.ProductName, AveragePrice.AveragePrice, CustomerCity.City
+FROM Products p
+JOIN (SELECT TOP 5 od.ProductID, RANK() OVER(ORDER BY SUM(od.Quantity)DESC) AS Rank
+FROM [Order Details] od
+GROUP BY od.ProductID
+ORDER BY SUM(od.Quantity)DESC) Top5
+ON p.ProductID = Top5.ProductID
+JOIN (SELECT od.ProductID,SUM(od.Quantity*od.UnitPrice*od.Quantity)/SUM(od.Quantity) AS AveragePrice
+FROM [Order Details] od
+GROUP BY od.ProductID) AveragePrice
+ON AveragePrice.ProductID = p.ProductID
+JOIN (SELECT od.ProductID, c.City, RANK( ) OVER(PARTITION BY od.ProductID ORDER BY SUM(od.Quantity) DESC) AS Rank, SUM(od.Quantity) as sum
+FROM Orders o
+JOIN [Order Details] od
+ON o.OrderID = od.OrderID
+Join Customers c
+ON c.CustomerID = o.CustomerID
+GROUP BY od.ProductID,  c.City) CustomerCity
+ON CustomerCity.ProductID = p.ProductID
+WHERE CustomerCity.Rank = 1
+ORDER BY Top5.Rank
+
+
+--9.A
+SELECT DISTINCT e.City
+FROM Employees e
+WHERE e.City NOT IN (
+    SELECT DISTINCT c.City 
+    FROM Customers c
+    JOIN Orders o
+    ON c.CustomerID = o.CustomerID
+
+)
+
+
+--9.B
+SELECT DISTINCT e.City
+FROM Customers c
+JOIN Orders o
+ON C.CustomerID = o.CustomerID
+RIGHT JOIN Employees e
+ON e.City = c.City
+WHERE O.CustomerID is NULL AND e.City IS NOT NULL
+
+
+--10
+--employee sold most
+SELECT TOP 1 e.city
+FROM Orders o
+JOIN Employees e
+ON o.EmployeeID = e.EmployeeID
+GROUP BY e.City
+
+--customer order most
+
+SELECT TOP 1 c.city
+FROM Orders o
+JOIN Customers c
+ON o.CustomerID = c.CustomerID
+GROUP BY c.City
+
+--Final:
+SELECT CityOrderMost.City
+FROM (SELECT TOP 1 e.city
+FROM Orders o
+JOIN Employees e
+ON o.EmployeeID = e.EmployeeID
+GROUP BY e.City) CitySellMost
+JOIN (SELECT TOP 1 c.city
+FROM Orders o
+JOIN Customers c
+ON o.CustomerID = c.CustomerID
+GROUP BY c.City) CityOrderMost
+ON CitySellMost.City = CityOrderMost.City
+
+--11
+
+DELETE FROM TableName
+WHERE pk in (
+SELECT t1.id 
+FROM TableName t1
+JOIN TableName t2
+ON t1.DuplicatedColumn1 = t2.DuplicatedColumn1 AND t1.DuplicatedColumn2 = t2.DuplicatedColumn2
+where t1.pk != t2.pk
+)
